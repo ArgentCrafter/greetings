a75ee096e37d8d7b52725662051aa5fcf3be1373
Jaden <Jaden_Hope@yahoo.com>
2021-05-18 10:39:02 +0200

    extra unit tests written

--- greet.js
+++ greet.js
@@ -6,17 +6,24 @@ const counter = document.getElementById("counter");
 
 let greet = greetFunctions();
 let regEx = /[a-z]/i;
+var timeOut = setTimeout(function () { display.innerHTML = "" }, 5000);
 
 function counterLoad() {
     counter.innerHTML = Object.keys(JSON.parse(localStorage.getItem("names"))).length;
 };
 
 btnGreet.addEventListener("click", function () {
+    clearTimeout(timeOut);
     var checkedBtn = document.querySelector("input[name='langInput']:checked");
     display.style.color = "black";
 
     if (nameInputElement.value.length > 10) {
         display.innerHTML = "Please enter a valid name.";
+        display.style.color = "red";
+        nameInputElement.value = "";
+        setTimeout(function () { display.innerHTML = "" }, 5000);
+    } else if (!regEx.test(nameInputElement.value) && !checkedBtn) {
+        display.innerHTML = "Please enter a valid name and select a language.";
         display.style.color = "red";
         nameInputElement.value = "";
         setTimeout(function () { display.innerHTML = "" }, 5000);
--- tests/functions.test.js
+++ tests/functions.test.js
@@ -3,4 +3,13 @@ describe("Factory function tests:", function() {
         let greet = greetFunctions();
         assert.equal(greet.displayString("jADeN", "Hello, "), "Hello, Jaden!");
     })
+    it("The displayString function should return 'Molo, Steven!' when given input = STEVEN and input2 = 'Molo, '", function () {
+        let greet = greetFunctions();
+        assert.equal(greet.displayString("STEVEN", "Molo, "), "Molo, Steven!");
+    })
+    it("the btnGreet function should return an object with the 'jaden' key holding a value of 0 when given input='jAdEN'", function() {
+        let greet = greetFunctions();
+        greet.btnGreetClicked("jAdEN");
+        assert.equal(JSON.stringify(greet.getGreetedNames()), '{"jaden":1}')
+    })
 });
