e73544cfd7441311ea780a09041d852150a733c7
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-05-21 16:37:37 +0200

    skim

--- functions.js
+++ functions.js
@@ -1,30 +1,31 @@
+// eslint-disable-next-line no-unused-vars
 function greetFunctions() {
-    var greetedNames = {};
+  let greetedNames = {};
 
-    function setGreetedNames(input){
-        greetedNames = input;
+  function setGreetedNames(input) {
+    greetedNames = input;
+  }
+
+  function btnGreetClicked(input) {
+    if (greetedNames[input.toLowerCase()] === undefined) {
+      greetedNames[input.toLowerCase()] = 1;
+    } else {
+      greetedNames[input.toLowerCase()]++;
     }
+  }
 
-    function btnGreetClicked(input) {
-        if (greetedNames[input.toLowerCase()] === undefined) {
-            greetedNames[input.toLowerCase()] = 1 ;
-        }else{
-            greetedNames[input.toLowerCase()]++;
-        }
-    }
+  function getGreetedNames() {
+    return greetedNames;
+  }
 
-    function getGreetedNames(){
-        return greetedNames;
-    }
+  function displayString(input, input2) {
+    return `${input2}${input[0].toUpperCase() + input.slice(1).toLowerCase()}!`;
+  }
 
-    function displayString(input, input2){
-        return input2 + (input[0].toUpperCase() + input.slice(1).toLowerCase() + "!");
-    }
-
-    return{
+  return {
     btnGreetClicked,
     getGreetedNames,
     displayString,
-    setGreetedNames
-    }
+    setGreetedNames,
+  };
 }
--- greet.js
+++ greet.js
@@ -1,57 +1,57 @@
-const nameInputElement = document.getElementById("nameInput");
-const btnGreet = document.getElementById("greetMe");
-const btnReset = document.getElementById("reset");
-const display = document.getElementById("displayMessage");
-const counter = document.getElementById("counter");
+const nameInputElement = document.getElementById('nameInput');
+const btnGreet = document.getElementById('greetMe');
+const btnReset = document.getElementById('reset');
+const display = document.getElementById('displayMessage');
+const counter = document.getElementById('counter');
 
-let greet = greetFunctions();
-let regEx = /[a-z]/i;
-var timeOut = setTimeout(function () { display.innerHTML = "" }, 5000);
+const greet = greetFunctions();
+const regEx = /[a-z]/i;
+const timeOut = setTimeout(() => { display.innerHTML = ''; }, 5000);
 
-function counterLoad() {
-    counter.innerHTML = Object.keys(JSON.parse(localStorage.getItem("names"))).length;
-};
-
-btnGreet.addEventListener("click", function () {
-    clearTimeout(timeOut);
-    var checkedBtn = document.querySelector("input[name='langInput']:checked");
-    display.style.color = "black";
-
-    if (nameInputElement.value.length > 15) {
-        display.innerHTML = "Please enter a valid name.";
-        display.style.color = "red";
-        nameInputElement.value = "";
-        setTimeout(function () { display.innerHTML = "" }, 5000);
-    } else if (!regEx.test(nameInputElement.value) && !checkedBtn) {
-        display.innerHTML = "Please enter a valid name and select a language.";
-        display.style.color = "red";
-        nameInputElement.value = "";
-        setTimeout(function () { display.innerHTML = "" }, 5000);
-    } else if (!regEx.test(nameInputElement.value)) {
-        display.innerHTML = "Please enter a valid name.";
-        display.style.color = "red";
-        nameInputElement.value = "";
-        setTimeout(function () { display.innerHTML = "" }, 5000);
-    } else if (!checkedBtn) {
-        display.innerHTML = "Please select a language.";
-        display.style.color = "red";
-        nameInputElement.value = "";
-        setTimeout(function () { display.innerHTML = "" }, 5000);
-    } else {
-        if (localStorage["names"]) {
-            greet.setGreetedNames(JSON.parse(localStorage.getItem("names")));
-        }
-
-        greet.btnGreetClicked(nameInputElement.value);
-        localStorage.setItem("names", JSON.stringify(greet.getGreetedNames()));
-
-        display.innerHTML = greet.displayString(nameInputElement.value, checkedBtn.value);
-        nameInputElement.value = "";
-    }
-    counter.innerHTML = Object.keys(JSON.parse(localStorage.getItem("names"))).length;
+document.body.onload(() => {
+  counter.innerHTML = Object.keys(JSON.parse(localStorage.getItem('names'))).length;
 });
 
-btnReset.addEventListener("click", function () {
-    localStorage.clear();
-    location.reload()
-})
+btnGreet.addEventListener('click', () => {
+  clearTimeout(timeOut);
+  const checkedBtn = document.querySelector("input[name='langInput']:checked");
+  display.style.color = 'black';
+
+  if (nameInputElement.value.length > 15) {
+    display.innerHTML = 'Please enter a valid name.';
+    display.style.color = 'red';
+    nameInputElement.value = '';
+    setTimeout(() => { display.innerHTML = ''; }, 5000);
+  } else if (!regEx.test(nameInputElement.value) && !checkedBtn) {
+    display.innerHTML = 'Please enter a valid name and select a language.';
+    display.style.color = 'red';
+    nameInputElement.value = '';
+    setTimeout(() => { display.innerHTML = ''; }, 5000);
+  } else if (!regEx.test(nameInputElement.value)) {
+    display.innerHTML = 'Please enter a valid name.';
+    display.style.color = 'red';
+    nameInputElement.value = '';
+    setTimeout(() => { display.innerHTML = ''; }, 5000);
+  } else if (!checkedBtn) {
+    display.innerHTML = 'Please select a language.';
+    display.style.color = 'red';
+    nameInputElement.value = '';
+    setTimeout(() => { display.innerHTML = ''; }, 5000);
+  } else {
+    if (localStorage.names) {
+      greet.setGreetedNames(JSON.parse(localStorage.getItem('names')));
+    }
+
+    greet.btnGreetClicked(nameInputElement.value);
+    localStorage.setItem('names', JSON.stringify(greet.getGreetedNames()));
+
+    display.innerHTML = greet.displayString(nameInputElement.value, checkedBtn.value);
+    nameInputElement.value = '';
+  }
+  counter.innerHTML = Object.keys(JSON.parse(localStorage.getItem('names'))).length;
+});
+
+btnReset.addEventListener('click', () => {
+  localStorage.clear();
+  Location.reload();
+});
